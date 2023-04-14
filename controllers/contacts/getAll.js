648176921c53ef5493649dcf274b6178c63c9f34
YuliiaGagina <ygaginazy@gmail.com>
2023-04-14 20:13:58 +0300

    remake third homework

---
+++ controllers/contacts/getAll.js
@@ -0,0 +1,14 @@
+const { Contact } = require("../../models/contact");
+
+const getAll = async (_, res) => {
+  const result = await Contact.find();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
+};
+
+module.exports = getAll;
---
+++ helpers/index.js
@@ -0,0 +1,7 @@
+const handleSchemaValidationErrors = require("./handleSchemaErrors");
+const requestError = require("./requestError");
+
+module.exports = {
+  handleSchemaValidationErrors,
+  requestError,
+};
